72639aeb5f32a5f76f5d3438fc05ee6473c18f29
ninnih <contact@ninnihdesign.com>
2020-08-17 16:52:51 +0200

    Fix

--- client/src/components/AboutContent/AboutContent.jsx
+++ client/src/components/AboutContent/AboutContent.jsx
@@ -14,9 +14,9 @@ const AboutContent = ({ aboutInfo }) => {
         <section className="content__about__info">
           <section className="information">
             <article><h2 className="information__name">{aboutInfo.name}</h2></article>
-            <h3 className="information__text">{aboutInfo.city}</h3>
+            <article className="information__text"><h3 className="information__text__header">Location: </h3><p className="information__text__paragraph">{aboutInfo.city}</p></article>
             <article className="information__text"><h3 className="information__text__header">About: </h3><p className="information__text__paragraph">{aboutInfo.about}</p></article>
-            <article className="information__text"><h3 className="information__text__header">Tech: </h3><p className="information__text__paragraph"> {aboutInfo.Tech}</p></article>
+            <article className="information__text information__text--tech"><h3 className="information__text__header">Tech: </h3><p className="information__text__paragraph"> {aboutInfo.Tech}</p></article>
             <article className="information__text"><h3 className="information__text__header">Skills: </h3><p className="information__text__paragraph"> {aboutInfo.skills}</p></article>
             <article className="information__text"><h3 className="information__text__header">Languages: </h3><p className="information__text__paragraph"> {aboutInfo.languages}</p></article>
             <article className="information__text"><h3 className="information__text__header">Interests: </h3><p className="information__text__paragraph"> {aboutInfo.interests}</p></article>
