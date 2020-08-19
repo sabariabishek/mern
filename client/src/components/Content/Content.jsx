230fcae9a4cbbc70d2e4260ee212071cdd02d9d7
ninnih <contact@ninnihdesign.com>
2020-08-19 22:20:51 +0200

    Small changes

--- client/src/components/Content/Content.jsx
+++ client/src/components/Content/Content.jsx
@@ -14,6 +14,7 @@ function Content({ projectData, projectIndex, openModal, clickCloseModal, clicke
               e.preventDefault();
               openModal(e.currentTarget, index)
               }}>
+              <div className="content__item__hover"></div>
               <div className="content__item__header">
                 <div className="content__item__header__title">
                   <h2 className="content__item__header__title__text">{item.projectName}</h2>
@@ -31,7 +32,6 @@ function Content({ projectData, projectIndex, openModal, clickCloseModal, clicke
               <div className="content__item__image">
                 <img src={item.image} alt="" srcSet="" className="content__item__image__img"/>
               </div>
-              <div className="content__item__hover"></div>
             </article>
           );
         })}
