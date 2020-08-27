221c33b769534d035f90d3ac957ad97770f36a61
ninnih <contact@ninnihdesign.com>
2020-08-27 20:09:58 +0200

    Sidebar and content fixes

--- client/src/components/Aside/Aside.jsx
+++ client/src/components/Aside/Aside.jsx
@@ -7,7 +7,8 @@ const Aside = ({ aboutInfo }) => {
   return (
     <aside className="sidebar">
       <section className="sidebar__description">
-        <p className="sidebar__description__p">"{aboutInfo.about}"</p>
+        <p className="sidebar__description__p">{aboutInfo.about}</p>
+        <p className="sidebar__description__p sidebar__description__p--salt">{aboutInfo.salt}</p>
       </section>
     </aside>
   );
