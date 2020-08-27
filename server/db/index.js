30a50aaa5c43d7e772a1226919e528b34d5975eb
ninnih <contact@ninnihdesign.com>
2020-08-27 11:45:14 +0200

    db change

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const psw = require('./psw.js');
 
+const dbname = 'new';
+const password = psw;
+const uri = `mongodb+srv://ninni:${password}@cluster0.vgdky.azure.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const devuri = 'mongodb://127.0.0.1:27017/new'
 mongoose
-    .connect('mongodb://127.0.0.1:27017/new', { useNewUrlParser: true })
+    .connect(uri, { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
