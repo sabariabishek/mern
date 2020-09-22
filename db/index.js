5f0ee73438b78880b4c0a4eed134a3e7b1ebb8ff
ninnih <contact@ninnihdesign.com>
2020-09-22 13:44:25 +0200

    Env variables and heroku fix

--- db/index.js
+++ db/index.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
-const psw = require('./psw.js');
-
-const dbname = 'new';
-const password = psw;
-const uri = `mongodb+srv://ninni:${password}@cluster0.vgdky.azure.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-const devuri = 'mongodb://127.0.0.1:27017/new'
+const uri = require('./psw.js').mongoURI;
 
 mongoose
     .connect(uri, { useNewUrlParser: true })
